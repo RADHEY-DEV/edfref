2551f71e7e1433d4f4ca0e1360959e9f07cbd9ec
radhey-molsoft <himali.baps@gmail.com>
2024-12-04 15:23:53 -0500

    add

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ensure the correct permissions for the startup script
-chmod +x startup.sh
-
-# Run Streamlit app with the appropriate parameters
-streamlit run app.py --server.port=$PORT --server.headless=true --server.enableCORS=false
